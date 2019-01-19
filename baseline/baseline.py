97ac39b34f1bcf73faea2806607114780a32924c
Patrick Kahardipraja <patrick.khdipraja@gmail.com>
2019-01-19 11:32:50 +0100

    Added results for testing baseline with test data

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -26,17 +26,53 @@ df['Tweets'] = df.Tweets.str.replace('\.', '')
 # print(df.iloc[784]['Tweets'])
 # print(df.iloc[4459]['Tweets'])
 
-#Convert tweets into features and do one-hot encoding for label
-trueLabel = pd.get_dummies(df['Label'], prefix=['Label'])
+#Convert tweets into features and label 
+trueLabel = df['Label'].replace({'Label': {'negative' : 0, 'positive': 1, 'neutral':2}})
+Y = trueLabel.values
+
 tweets = df['Tweets']
 tweetsMat = tweets.iloc[:]
 tweetsMat = tweetsMat.values
 # print(tweetsMat.shape)
 count = CountVectorizer()
 bagOfWords = count.fit_transform(tweetsMat)
-X = bagOfWords.toarray()
+X = bagOfWords.toarray().astype(int)
 print(X.shape)
-
+print(Y.shape)
 #Show feature names (vocabulary)
-feature_names = count.get_feature_names()
-print(feature_names)
+# feature_names = count.get_feature_names()
+# print(feature_names)
+
+# Train classifier
+clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial').fit(X, Y)
+print("training finished")
+
+#Test on itself
+acc = clf.score(X, Y)
+
+#Test on all data
+test_df = pd.read_csv(dataPath, sep='\t', header=None, names=['ID', 'Label', 'Orig', 'Tweets'], quoting=csv.QUOTE_ALL, encoding='utf8')
+test_df = test_df.drop(['ID', 'Tweets'], axis = 1)
+test_df = test_df.dropna()
+
+test_df['Orig'] = test_df.Orig.str.replace('\"', '') #remove double quote
+test_df['Orig'] = test_df.Orig.str.replace('\'', '') #remove quote
+test_df['Orig'] = test_df.Orig.str.replace(':', '') #remove colon, careful of removing emoticons, must use regex!!
+test_df['Orig'] = test_df.Orig.str.replace('\,', '') #remove comma
+test_df['Orig'] = test_df.Orig.str.replace('\.', '')
+
+testLabel = test_df['Label'].replace({'Label': {'negative' : 0, 'positive': 1, 'neutral':2}})
+test_y = testLabel.iloc[:20000].values
+
+test_tweets = test_df['Orig']
+testtweetsMat = test_tweets.iloc[:20000]
+testtweetsMat = testtweetsMat.values
+print(testtweetsMat.shape)
+# count_test = CountVectorizer(vocabulary=count.get_feature_names(), )
+bagOfWords_test = count.transform(testtweetsMat)
+test_x = bagOfWords_test.toarray().astype(int)
+# print(bagOfWords_test)
+
+print(test_y.shape, test_x.shape)
+test_acc = clf.score(test_x, test_y)
+print(acc, test_acc)

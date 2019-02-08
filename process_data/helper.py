22019eb009cc75be141a690056468f2add067d13
rafi <rafiderberliner@googlemail.com>
2019-02-08 01:29:10 +0100

    Adding a preprocessing function, not sure it's needed...

--- process_data/helper.py
+++ process_data/helper.py
@@ -10,7 +10,7 @@ import html
 import nltk.corpus.reader.conll as conll
 from ekphrasis.classes.spellcorrect import SpellCorrector
 import matplotlib.pyplot as plt
-
+from nltk.corpus import stopwords
 
 def reduce_lengthening(text):
     """
@@ -347,17 +347,20 @@ def extract_range(iterable, start_range=None, end_range=None):
     return iterable[start_range:end_range]
 
 
-def get_tagged_sentences(folder, filename, file_extension=".csv", start_range=None, end_range=None):
+def get_tagged_sentences(folder, filename, file_extension=".csv", start_range=None, end_range=None, split_pos=True):
     """
 
     :param folder:     Folder to the tagged sentences
     :param filename: the file to parse
     :param file_extension: ending of the file toi be parsed
-    :return: three lists, one with the tokenized sentences, one with the tags,
-            one with tokenized sentences for testing
+    :param split_pos: if false, returns a list of documents, where each documents contains a tuple (word,pos), if true 2 separated lists (one list of words, one list of corresponding pos)
+    :return: one or 2 lists, see param split_pos
     """
     corpus = conll.ConllCorpusReader(folder, file_extension, ('words', 'pos'))
     tagged_sentences = corpus.tagged_sents(filename)
+
+    if not split_pos:
+        return tagged_sentences
 
     sentences_only = []
     tags_only = []
@@ -407,6 +410,40 @@ def build_pie_chart(data_frame_labels, chart_title="Label distribution in the Se
     plt.title(chart_title, bbox={'facecolor': '0.95', 'pad': 5})
     plt.savefig(filename)
 
+def pre_processing(tagged_sentence, pos_grouping={"V": ["V"], "A": ["A"], "N": ["N"], "R": ["R"]},
+                   default_pos="DEFAULT",
+                   stopwords=set(stopwords.words('english')), to_lower=True):
+    """
+    Apply some pre-processing on pre tagged sentences. Stopwords, POS Grouping, lowering case
+    :param tagged_sentence:
+    :param pos_grouping:
+    :param default_pos:
+    :param stopwords:
+    :param to_lower:
+    :return: tagged sentences with removed stopped words and different pos grouping
+    """
+
+    processed_sentences = []
+    for sentence in tagged_sentence:
+        new_sentence = []
+        for word_pos in sentence:
+            word = word_pos[0]
+            pos = word_pos[1]
+            group_found = False
+            if to_lower:
+                word = word.lower()
+            if word in stopwords:
+                continue
+            for pos_group_key, pos_group_values in pos_grouping.items():
+                if pos in pos_group_values:
+                    new_sentence.append((word, pos_group_key,))
+                    group_found = True
+                    break
+            # Fallback to avoid empty documents. Exemple tweet that does not contain any of our groups => just emoji + hashtag
+            if not group_found:
+                new_sentence.append((word, default_pos,))
+        processed_sentences.append(new_sentence)
+    return processed_sentences
 
 if __name__ == "__main__":
     parent_dir = Path(__file__).parents[1]

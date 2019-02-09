d2ddc1ee763edea54a6d1b949e6462dcf306c2d7
Patrick Kahardipraja <patrick.khdipraja@gmail.com>
2019-02-09 23:27:57 +0100

    Commented out pre_processing. Still buggy

--- features/ocfs.py
+++ features/ocfs.py
@@ -126,7 +126,7 @@ def main():
                                             end_range=END_RANGE, split_pos=False)
 
     pos_groups = {"V": ["V"], "A": ["A"], "N": ["N"], "R": ["R"]}
-    tagged_sentences = pre_processing(tagged_sentences, pos_grouping=pos_groups)
+    # tagged_sentences = pre_processing(tagged_sentences, pos_grouping=pos_groups)
 
     all_docs = []
     all_tags = []
